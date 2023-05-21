0f5dc4d7f51e582cbdec2b0edce8c33f34573368
Mazuzu33 <mazuki.wu@gmail.com>
2023-05-21 18:13:40 -0500

    Hey

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -6,6 +6,9 @@ package entity;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import pkg2dgame.GamePanel;
 import pkg2dgame.KeyHandler;
 
@@ -25,6 +28,7 @@ public class Player extends Entity{
         setDefaultValues();
         getPlayerImage();
     }
+    //sets defalt values for player
     public void setDefaultValues()
     {
         x = 100;
@@ -32,52 +36,96 @@ public class Player extends Entity{
         speed = 4;
         direction = "down";
     }
-    
+    //reads sprite images
     public void getPlayerImage()
     {
         try
         {
-            up1 = ImageIO.read(getClass().getResourceAsStream());
-            up2 = ImageIO.read(getClass().getResourceAsStream());
-            down1 = ImageIO.read(getClass().getResourceAsStream());
-            down2 = ImageIO.read(getClass().getResourceAsStream());
-            left1 = ImageIO.read(getClass().getResourceAsStream());
-            left2 = ImageIO.read(getClass().getResourceAsStream());
-            right1 = ImageIO.read(getClass().getResourceAsStream());
-            right2 = ImageIO.read(getClass().getResourceAsStream());
+            up1 = ImageIO.read(getClass().getResourceAsStream("/player/Blob Backward 1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/player/Blob Backward 2.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/player/Blob Forward 1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/player/Blob Forward 2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/player/Blob Left 1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/player/Blob Left 2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/player/Blob Right 1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/player/Blob Right 2.png"));
         }
         catch(IOException e)
         {
+            //prints where error happened
             e.printStackTrace();
         }
     }
+    //called 60 times a second - updates the current direction, location, and time only if a key is pressed
     public void update()
     {
-        if (keyH.upPressed == true)
+        if (keyH.upPressed == true || keyH.downPressed == true || keyH.leftPressed == true || keyH.rightPressed == true)
         {
-            direction = "up";
-            y -= speed;
-        }
-        else if (keyH.downPressed == true)
-        {
-            direction = "down";
-            y += speed;
-        }
-        else if (keyH.leftPressed == true)
-        {
-            direction = "left";
-            x -= speed;
-        }
-        else if (keyH.rightPressed == true)
-        {
-            direction = "right";
-            x += speed;
-        }
+            if (keyH.upPressed == true)
+            {
+                direction = "up";
+                y -= speed;
+            }
+            else if (keyH.downPressed == true)
+            {
+                direction = "down";
+                y += speed;
+            }
+            else if (keyH.leftPressed == true)
+            {
+                direction = "left";
+                x -= speed;
+            }
+            else if (keyH.rightPressed == true)
+            {
+                direction = "right";
+                x += speed;
+            }
+            //changes player image every 10 frames
+            spriteCounter++;
+            if(spriteCounter > 10)
+            {
+                if (spriteNum == 1)
+                    spriteNum = 2;
+                else if (spriteNum == 2)
+                    spriteNum = 1;
+                spriteCounter = 0;
+            }
+        }       
     }
+    
+    //draws image of chracter based on direction, time, and location
     public void draw(Graphics2D g2)
     {
-        g2.setColor(Color.white);
-         
-         g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+        BufferedImage image  = null;
+        if (direction.equals("up"))
+        {
+            if (spriteNum == 1)
+                image = up1;
+            else if (spriteNum == 2)
+                image = up2; 
+        }
+        else if (direction.equals("down"))
+        {
+            if (spriteNum == 1)
+                image = down1;
+            else if (spriteNum == 2)
+                image = down2;
+        }
+        else if (direction.equals("left"))
+        {
+            if (spriteNum == 1)
+                image = left1;
+            else if (spriteNum == 2)
+                image = left2; 
+        }
+        else if (direction.equals("right"))
+        {
+            if (spriteNum == 1)
+                image = right1;
+            else if (spriteNum == 2)
+                image = right2;  
+        }      
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
     }
 }
